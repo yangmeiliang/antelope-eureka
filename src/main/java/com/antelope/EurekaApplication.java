245d9fffcad1079e8f1df67055ee4c01b03c3904
yangmeiliang <yangmeiliang@dxy.cn>
2020-05-25 11:46:30 +0800

    init

---
+++ src/main/java/com/antelope/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.antelope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yaml
+ * @since 2020/5/25
+ */
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class);
+    }
+}
